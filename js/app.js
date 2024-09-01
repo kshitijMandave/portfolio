ebbbe151a8ee9cacbf8c481f9faab80428288ab4
kshitijMandave <kshitij.mandwe@gmail.com>
2024-09-02 00:02:12 +0530

    Addesd Slider Section

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// whenever our page is ready to use, this fuction will trigger automatically.
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+    });
+
+});
